46d0bc06c5a61a857ff7be1168b76a59ae105072
Ayush Kumar Patel <aksingh6224@gmail.com>
2024-10-06 15:02:54 +0530

    variables

---
+++ 1_variables.js
@@ -0,0 +1,12 @@
+const acountId = 1234
+let email = "aksihn@gmail.com"
+var accPass = "123"
+accountCit = "Muz"
+
+console.log(accountCit);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([acountId, email, accPass, accountCit]);
