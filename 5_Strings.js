61860eb99d5207f002a41d53c27accd728407f49
Ayush Kumar Patel <aksingh6224@gmail.com>
2024-10-06 15:05:37 +0530

    Strings

---
+++ 5_Strings.js
@@ -0,0 +1,9 @@
+const name = "Ayush"
+const age = 21
+
+//Old ways
+// console.log(name + age);
+
+//Using bactisk (String intercollision), we create place holders and inject variables directly 
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
